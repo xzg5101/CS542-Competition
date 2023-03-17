7b02f8df2a74af2e34d1e17b51f4562ac141a130
xzg5101 <guoxiaoyang11@outlook.com>
2023-03-17 19:19:05 -0400

    fix: fix grammar in prompt

--- transformer_test.py
+++ transformer_test.py
@@ -8,7 +8,7 @@ train_questions = json.load(open('training.json', encoding='utf-8')) # from the 
 # use first one as example
 q = train_questions[0]['question']+' Please answer a number.'
 
-prompt = train_questions[0]['question'] + ' Here are your options: ' + train_questions[0]['choices'] + ' the correct one is'
+prompt = train_questions[0]['question'] + ' Here are your options: ' + train_questions[0]['choices'] + '. The correct one is'
 print(prompt)
 
 # transformer pipeline
