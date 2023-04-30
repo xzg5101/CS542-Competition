5878e6d530295920bc1b5a44f1921d23c93f6f41
Zhi Zheng <gia.zheng83@gmail.com>
2023-04-30 16:31:09 -0400

    fix: print output

--- mc_inference.py
+++ mc_inference.py
@@ -59,6 +59,7 @@ def ft_pred(device, tokenizer, model, length, question):
 
     gen_text = text.replace(prompt_text, '').strip().replace('\n', '')
     # gen_ans = 'yes' if gen_text[0:3] == 'yes' else 'no'
+    print("The output is " + gen_text)
     gen_ans = gen_text[0:1]
 
     if gen_text[0:1] in letters:
