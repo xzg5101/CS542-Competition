8e00db935c1b47719f0e2b1aaedf14184ac98cee
xzg5101 <guoxiaoyang11@outlook.com>
2023-05-01 22:30:15 -0400

    Update bool_inference.py

--- bool_inference.py
+++ bool_inference.py
@@ -50,7 +50,6 @@ def ft_pred(device, tokenizer, model, length, question):
     
     encoded_prompt = tokenizer.encode(prompt_text, add_special_tokens=False, return_tensors="pt")
     encoded_prompt = encoded_prompt.to(device)
-    print("encoded input type is", type(encoded_prompt[0]), " length is ", len(encoded_prompt[0]))
     outputs = model.generate(
         input_ids = encoded_prompt,
         max_length = len(encoded_prompt[0]) + ANS_LEN,
