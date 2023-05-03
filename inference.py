3db254bca1565c32b518a53dc2367299f6434436
xzg5101 <guoxiaoyang11@outlook.com>
2023-05-02 22:03:09 -0400

    combine mc and bool in inference

--- inference.py
+++ inference.py
@@ -13,9 +13,12 @@ from transformers import (
     GPT2Tokenizer,
 )
 
-MAX_LENGTH = int(10000)  # Hardcoded max length to avoid infinite loop
-LENGTH = 100
+from utils import clean_background
+from utils import MAX_LENGTH, LENGTH, ANS_LEN
 
+bool_model_path = "bool_fine_tuning2"
+mc_model_path = 'choice_fine_tuning'
+letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 autocast_questions = json.load(open('autocast_questions.json', encoding='utf-8')) # from the Autocast dataset
 test_questions = json.load(open('autocast_competition_test_set.json', encoding='utf-8'))
 test_ids = [q['id'] for q in test_questions]
@@ -31,84 +34,143 @@ def adjust_length_to_model(length, max_sequence_length):
 
 def ft_init():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    tokenizer = GPT2Tokenizer.from_pretrained("output")
-    model = GPT2LMHeadModel.from_pretrained("output")
-    model.to(device)
-    length = adjust_length_to_model(LENGTH, max_sequence_length=model.config.max_position_embeddings)
-    return device, tokenizer, model, length
 
-def ft_pred(device, tokenizer, model, length, question):
-    prompt_text = question["question"] + " The answer is"
+    bool_tokenizer = GPT2Tokenizer.from_pretrained(bool_model_path)
+    bool_model = GPT2LMHeadModel.from_pretrained(bool_model_path)
+    bool_model.to(device)
+    
+    mc_tokenizer = GPT2Tokenizer.from_pretrained(mc_model_path)
+    mc_model = GPT2LMHeadModel.from_pretrained(mc_model_path)
+    return device, bool_tokenizer, bool_model, mc_tokenizer, mc_model
+
+def ft_pred_bool(device, tokenizer, model, question):
+    tags = ''
+    if len(question['tags']) > 0:
+        tags = "This question is about " + ", ". join(question['tags']) + '. '
+    bg = clean_background(str(question['background']))
+
+    #prompt_text = tags + bg + str(question['question']) + " The correct answer is"
+    prompt_text = str(question['question']) + " The correct answer is"
     encoded_prompt = tokenizer.encode(prompt_text, add_special_tokens=False, return_tensors="pt")
     encoded_prompt = encoded_prompt.to(device)
-    output_sequences = model.generate(
+    outputs = model.generate(
         input_ids = encoded_prompt,
-        max_length = length,
+        max_length = encoded_prompt.shape[1] + ANS_LEN,
         temperature = 1.0,
         top_k = 0,
         top_p = 0.9,
         repetition_penalty = 1.0,
         pad_token_id = 50256,
+        return_dict_in_generate=True,
+        output_scores=True,
     )
 
-    generated_sequence = output_sequences[0].tolist()
+    output_seq = outputs.sequences
+    generated_sequence = output_seq[0].tolist()
     text = tokenizer.decode(generated_sequence, clean_up_tokenization_spaces=True)
-    #text = text[: text.find(stop_token) if args.stop_token else None]
 
-    gen_text = text.replace(prompt_text, '').strip().replace('\n', '')
-    gen_ans = 'yes' if gen_text[0:3] == 'yes' else 'no'
+    input_length = encoded_prompt.shape[1]
+    transition_scores = model.compute_transition_scores(outputs.sequences, outputs.scores, normalize_logits=True)
+    generated_tokens = outputs.sequences[:, input_length:]
 
-    if gen_text[0:3] == 'yes':
+    confident = 0
+    gen_ans = 'unknown'
+    meet_answer = False
+    for tok, score in zip(generated_tokens[0], transition_scores[0]):
+        if tokenizer.decode(tok).strip() in ['yes', 'no', 'positive', 'negative']:
+            print(f"|{tokenizer.decode(tok):8s} | {score.numpy():.4f} | {np.exp(score.numpy()):.4f}")
+            confident = np.exp(score.numpy())
+            gen_ans = tokenizer.decode(tok).strip()
+            print(f"answer: {tokenizer.decode(tok):8s} | {confident:.4f}")
+            meet_answer = True
+            break
+    if gen_ans in ['yes', 'positive']:
         gen_ans = 'yes'
-    elif gen_text[0:2] == 'no':
-        gen_ans = 'no'
     else:
-        print('unknown answer:', gen_text)
-
-    prob_ans = np.array([0.0, 1.0]) if gen_text[0:3] == 'yes' else np.array([1.0, 0.0])
-
-    pred_idx = 1 if gen_text[0:3] == 'yes' else 0
+        gen_ans = 'no'
+     
+    if not meet_answer:
+        print('unexpected answer:', text)
+        return np.array([0.5, 0.5])
+    print('final answer is ', gen_ans)
+    pred_idx = 1 if gen_ans == 'yes' else 0
     pred = np.ones(2)
-    pred[pred_idx] += 8
+    pred[pred_idx] += confident
+    print( pred / pred.sum() )
+    return  pred / pred.sum()
 
-    return gen_ans, prob_ans
+def ft_pred_mc(device, tokenizer, model, question):
+    tags = ''
+    if len(question['tags']) > 0:
+        tags = "This question is about " + ", ". join(question['tags']) + '. '
+    bg = clean_background(str(question['background']))
+    trimmed_choices = question['choices'][0:26]
+    choices_prompt = [i + ":" + str(j) for i, j in zip(letters[0:len(trimmed_choices)], trimmed_choices)]
+    prompt_text = tags + bg + question["question"] + ". You have following choices: " + '; '.join(choices_prompt) + ". The correct choice is"
+    encoded_prompt = tokenizer.encode(prompt_text, add_special_tokens=False, return_tensors="pt")
+    encoded_prompt = encoded_prompt.to(device)
+    outputs = model.generate(
+        temperature = 1.0,
+        top_k = 0,
+        top_p = 0.9,
+        input_ids = encoded_prompt,
+        max_length = encoded_prompt.shape[1] + ANS_LEN,
+        pad_token_id = 50256,
+        return_dict_in_generate=True,
+        output_scores=True,
+    )
+    output_seq = outputs.sequences
+    generated_sequence = output_seq[0].tolist()
+    text = tokenizer.decode(generated_sequence, clean_up_tokenization_spaces=True)
+    
+    input_length = encoded_prompt.shape[1]
+    transition_scores = model.compute_transition_scores(outputs.sequences, outputs.scores, normalize_logits=True)
+    generated_tokens = outputs.sequences[:, input_length:]
+
+    confident = 0
+    gen_ans = 'unknown'
+
+    meet_answer = False
+
+    for tok, score in zip(generated_tokens[0], transition_scores[0]):
+        if tokenizer.decode(tok).strip() in letters:
+            print(f"|{tokenizer.decode(tok):8s} | {score.numpy():.4f} | {np.exp(score.numpy()):.4f}")
+            confident = np.exp(score.numpy())
+            gen_ans = tokenizer.decode(tok).strip()
+            print(f"answer: {tokenizer.decode(tok):8s} | {confident:.4f}")
+            meet_answer = True
+            break
+    
+    if not meet_answer:
+        print('unexpected answer:', text)
+        even_ans = np.ones(len(question['choices']))
+        return even_ans / even_ans.sum()
+    
+    print('final answer is ', gen_ans)
+    pred = np.ones(len(question['choices']))
+    pred_idx = letters.find(gen_ans)
+    pred[pred_idx] += confident
+    print( pred / pred.sum() )
+    return pred / pred.sum()
 
 def ft_model(question):
     if question['qtype'] == 't/f':
-        ft_ans, ft_prob = ft_pred(device, tokenizer, model, length, question)
+        ft_prob = ft_pred_bool(device, bool_tokenizer, bool_model, question)
         return ft_prob
     elif question['qtype'] == 'mc':
-        pred_idx = np.argmax(np.random.random(size=len(question['choices'])))
-        pred = np.ones(len(question['choices']))
-        pred[pred_idx] += 1e-5
-        return pred / pred.sum()
+        ft_prob = ft_pred_mc(device, bool_tokenizer, bool_model, question)
+        return ft_prob
     elif question['qtype'] == 'num':
         return 0.5
 
-def calibrated_random_baseline_model(question):
-    if question['qtype'] == 't/f':
-        pred_idx = np.argmax(np.random.random(size=2))
-        pred = np.ones(2)
-        pred[pred_idx] += 1e-5
-        return pred / pred.sum()
-    elif question['qtype'] == 'mc':
-        pred_idx = np.argmax(np.random.random(size=len(question['choices'])))
-        pred = np.ones(len(question['choices']))
-        pred[pred_idx] += 1e-5
-        return pred / pred.sum()
-    elif question['qtype'] == 'num':
-        return 0.5
-
-def brier_score(probabilities, answer_probabilities):
-    return ((probabilities - answer_probabilities) ** 2).sum() / 2
 
 
-device, tokenizer, model, length = ft_init()
+device, bool_tokenizer, bool_model, mc_tokenizer, mc_model = ft_init()
 
 
 preds = []
 for idx, question in enumerate(test_questions):
-    print(f"{idx}/{len(test_questions)}")
+    print(f"\n{idx}/{len(test_questions)} type: {question['qtype']} ")
     preds.append(ft_model(question))
 
 if not os.path.exists('submission'):
